b713f04bf70ae9f0f0b4b98f070932e6af62e917
arnav-bhawar <64008302+arnav-bhawar@users.noreply.github.com>
2020-05-09 22:47:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var thickness,wall;
+var speed,weight,bullet;
+
+function setup() {
+  createCanvas(1600 ,400);
+ // createSprite(400, 200, 50, 50);
+  speed=random(223,321);
+  weight=random(30,52);
+  bullet = createSprite(50,200,100,50);
+ // bullet.shapeColor=color(128,0,128);
+  wall=createSprite(1200,200,thickness,height/2);   
+  wall.shapeColor=color(80,80,80);
+  bullet.velocityX=speed;
+  bullet.shapeColor=color(255,255,255);
+  thickness=random(22,83);
+
+}
+
+function hasCollided(bullet,wall)
+{
+  bulletRightEdge=bullet.x +bullet.width;
+  wallLeftEdge=wall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true
+  }
+  return false;
+}
+
+function draw() {
+  background(0,0,0);  
+  
+  if(hasCollided(bullet,wall))
+  {
+    bullet.velocityX=0;  
+    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
+    textSize(20);
+   
+   
+
+    if(damage>10){
+      wall.shapeColor=color(255,0,0);
+      fill("red");
+      text("Damage = "+damage,200,200);
+      text("Ineffective",200,220);
+    }
+
+    if(damage<10)
+    {wall.shapeColor=color(0,255,0)
+      fill("green");
+      text("Damage = "+damage,200,200);
+      text("effective",200,220);
+      
+  
+    }
+   
+  }
+
+  drawSprites();
+}
